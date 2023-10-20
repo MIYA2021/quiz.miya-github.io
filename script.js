34ef822370bd8e906c438d6e9a6af2b12c7c337c
MIYA2021 <80123162+MIYA2021@users.noreply.github.com>
2023-10-20 19:34:39 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,316 @@
+var questions = [
+    {
+        question: "ミッキーマウスとミニーマウスの誕生日は？",
+        choices: ["5/25", "9/5", "11/18"],
+        correctIndex: 2,
+        explanation: " "
+
+    },
+    {
+        question: "シンデレラの魔法使いがシンデレラに唱える呪文といえば",
+        choices: ["ピクデピクデプー", "ビビデバビデブー", "エクスペクトパトローナム"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "ミッキーの恋人は？",
+        choices: ["ミニーマウス", "デイジーダック", "ダッフィー"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "クマのプーさんが好きなのは？？",
+        choices: ["砂糖", "ハチミツ", "ハチの巣"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "エルサはどこの国の女王ですか",
+        choices: ["アレンディエール", "アレンデール", "アレンドゥール"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "シンデレラが乗っていた馬車はなんの食べ物？",
+        choices: ["りんご", "かぼちゃ", "メロン"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "白雪姫が悪いか魔女からもらって食べてしまったのは？",
+        choices: ["ドーナッツ", "みかん", "りんご"],
+        correctIndex: 2,
+        explanation: " "
+    },
+    {
+        question: "ウッディの靴に書いてある名前は？",
+        choices: ["ウッディの名前", "アンディの名前", "アンディの誕生日"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "バズ・ライトイヤーの敵キャラといえばどれ？",
+        choices: ["ヘレックス", "ギーグ", "ザーグ"],
+        correctIndex: 2,
+        explanation: " "
+    },
+    {
+        question: "モンスターズインクに出てくる女の子の名前は？",
+        choices: ["ビー", "ブー", "ベー"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションの館のなかにある時計は、12の部分はどれか？",
+        choices: ["15", "13", "19"],
+        correctIndex: 1,
+        explanation: "13は呪いの数字と言われているから!!"
+    },
+    {
+        question: "ホーンテッドマンションがあるテーマランドは？",
+        choices: ["ファンタジーランド", "トゥモローランド", "ウエスタンランド"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "1番古いホーンテッドマンションがあるパークは？",
+        choices: ["カルフォルニア", "東京", "パリ"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションの待ち時間でないものは？",
+        choices: ["9分", "13分", "4分"],
+        correctIndex: 0,
+        explanation: "『９』だと苦しいという語呂合わせになってしまうから"
+    },
+    {
+        question: "ホーンテッドマンションの黒い乗り物の名前は？",
+        choices: ["ブラックバギー", "ドゥームバギー", "ゴーストバギー"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションには管理人がいるが、連れている動物は何か",
+        choices: ["猫", "犬", "鳥"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションの入り口は何口か。",
+        choices: ["正門入り口", "庭の入り口", "使用人出入り口"],
+        correctIndex: 2,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションのアトラクション乗り場のベルトコンベアにあるイラストは何か。",
+        choices: ["コウモリ", "蜘蛛", "スカル"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションには隠れたキャラクターがいるが、ないものはどれか。",
+        choices: ["ミッキー", "ミニー", "ドナルド"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "シンデレラ城の屋根の先端は何色？",
+        choices: ["白", "金", "青"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "東京ディズニーランドの敷地はオープン以前はどんな場所だった？",
+        choices: ["公園", "ゴミ処理場", "海"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "くまのプーさんに出てくる豚のキャラクターの名前は？",
+        choices: ["ピグレット", "ティガー", "イーヨー"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "ピグレットの好物は？",
+        choices: ["アザミの花", "どんぐり", "プーさんと同じハチミツ"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "リスのキャラクターチップとデールの、デールの鼻は何色でしょう？",
+        choices: ["黒", "黄色", "赤"],
+        correctIndex: 2,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションに住んでいる幽霊の数は？",
+        choices: ["10", "555", "999"],
+        correctIndex: 2,
+        explanation: " "
+    },
+    {
+        question: "ディズニーランドのクリッターカントリーにあるレストランの名前は？",
+        choices: ["グラン・マサラ", "グランマ・サラ", "グランマサ・ラ"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "次のホテルのうち、ディズニーホテルはどれ？",
+        choices: ["セレブレーションホテル", "シェラトン・グランデトーキョーベイホテル", "オリエンタルホテル東京ベイ"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "ピノキオを作ったおじいさんの名前は",
+        choices: ["ピボット", "ゼペット", "サラット"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "では、その黒いライドは、ホリデーナイトメアの期間中は何と呼ばれるか",
+        choices: ["黒のソリ", "闇のソリ", "やばいソリ"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "ホーンテッドマンションでゲストを案内するゴーストの名は？",
+        choices: ["グリニングゴースト", "マダムレオタ", "ゴーストホスト"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "プーさんのハニーハントで待ち時間にないものは何？",
+        choices: ["8分", "14分", "88分"],
+        correctIndex: 2,
+        explanation: "8は『ハチ』、14は『イーヨー』なので88は無い"
+    },  
+    {
+        question: "アリスに出てくるハートの女王は白いばらを染めろと言ったがなんの色？",
+        choices: ["赤", "青", "黒"],
+        correctIndex: 0,
+        explanation: " "
+    },
+    {
+        question: "イッツ・ア・スモールワルドに出てくるキャラクターはどれ？",
+        choices: ["プルート", "スティッチ", "ウッディ"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "ベイマックスのハッピーライドの曲数は？",
+        choices: ["3", "9", "6"],
+        correctIndex: 2,
+        explanation: " "
+    },
+    {
+        question: "ピーターパンに出てくるフック船長が苦手な生き物は何？",
+        choices: ["犬", "ワニ", "ライオン"],
+        correctIndex: 1,
+        explanation: " "
+    },
+    {
+        question: "白雪姫に出てくる小人は何人でしょうか？",
+        choices: ["7", "5", "10"],
+        correctIndex: 0,
+        explanation: " "
+    },
+];
+
+var currentQuestionIndex;
+var quizCount;
+var correctCount;
+
+function initQuiz() {
+    currentQuestionIndex = -1;
+    quizCount = 0;
+    correctCount = 0;
+    nextQuestion();
+}
+
+function nextQuestion() {
+    // クイズ回数が5に達したら終了
+    if (quizCount >= 5) {
+        var resetConfirmation = confirm("クイズが終了しました。リセットしますか？\n正解した回数: " + correctCount);
+        if (resetConfirmation) {
+            // リセットの場合
+            initQuiz();
+        } else {
+            // リセットしない場合
+            alert("お疲れ様でした！\n正解した回数: " + correctCount);
+        }
+        return;
+    }
+
+    var randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * questions.length);
+    } while (randomIndex === currentQuestionIndex);
+
+    currentQuestionIndex = randomIndex;
+    displayQuestion();
+
+    // クイズ回数を増加
+    quizCount++;
+    // HTML にクイズ回数を反映
+    document.getElementById('count').textContent = quizCount;
+}
+
+function displayQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+
+    // 正解の選択肢を保存
+    var correctChoice = currentQuestion.choices[currentQuestion.correctIndex];
+
+    // 選択肢をシャッフル
+    var shuffledChoices = shuffle(currentQuestion.choices);
+
+    document.getElementById('question').textContent = currentQuestion.question;
+
+    var choices = document.getElementsByClassName('choice');
+    for (var i = 0; i < choices.length; i++) {
+        choices[i].textContent = shuffledChoices[i];
+
+        // 正解の選択肢の場合は正しいインデックスを設定
+        if (shuffledChoices[i] === correctChoice) {
+            currentQuestion.correctIndex = i;
+        }
+    }
+}
+
+function checkAnswer(choiceIndex) {
+    var currentQuestion = questions[currentQuestionIndex];
+
+    if (choiceIndex === currentQuestion.correctIndex) {
+        // 正解の場合
+        alert("正解！\n" + currentQuestion.explanation);
+        correctCount++;
+    } else {
+        // 不正解の場合
+        var correctAnswer = currentQuestion.choices[currentQuestion.correctIndex];
+        alert("不正解...\n正解は: " + correctAnswer + "\n" + currentQuestion.explanation);
+    }
+
+    // 次の質問に進む
+    nextQuestion();
+}
+
+// クイズ初期化
+initQuiz();
+
+// Fisher-Yates シャッフルアルゴリズム
+function shuffle(array) {
+    var currentIndex = array.length, randomIndex, tempValue;
+  
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+  
+        tempValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = tempValue;
+    }
+  
+    return array;
+}
